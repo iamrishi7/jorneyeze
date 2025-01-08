4e5d72e843e4d695732ea2ec15b62012ccb44a89
sangam-cm <sangam@cloudmagician.in>
2025-01-08 22:46:40 +0530

    FIRST

---
+++ src/data/site.ts
@@ -0,0 +1,41 @@
+// Only for Astro page
+
+export const siteConfig = {
+  title: "Ultimate Astro",
+  description: "The Ultimate Multi-Framework Astro Template",
+};
+
+export const home = {
+  title: "We are\nComing Soon",
+  subtitle: "JOURNYEZE",
+  description:
+    "Adventure Awaits! Get ready to explore breathtaking destinations, unforgettable experiences, and hassle-free travel planning—all in one place. Our platform is launching soon to bring your dream journeys to life. Stay tuned!",
+  features: [
+    {
+      title: "Travel",
+      description: "🚌 Use our platform to reserve your seats for traveling",
+    },
+    {
+      title: "Activities",
+      description: "🎡 Have your own transit? No problem, just book the tickets for activities only.",
+    },
+    {
+      title: "Tour",
+      description: "🚀 Hate planning and all? We got you. We have tour packages ready for you",
+    },
+  ],
+  stats: [
+    {
+      value: "5+",
+      label: "Frameworks",
+    },
+    {
+      value: "10+",
+      label: "Components",
+    },
+    {
+      value: "99",
+      label: "Performance Score",
+    },
+  ],
+};
